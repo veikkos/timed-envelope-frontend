cd59dbd4bf4acc8957c66d23fe034c8b1140e815
Veikko Soininen <veikko.soininen@gmail.com>
2022-09-11 16:13:19 +0300

    Add Tailwind CSS. Further cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+const url = process.env.REACT_APP_ENVELOPE_BACKEND_URL;
+
 function App() {
+  console.log(url);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
