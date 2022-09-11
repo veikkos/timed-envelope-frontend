1c71ae252eac6292b67f5a1d33ced3a20223c653
Veikko Soininen <veikko.soininen@gmail.com>
2022-09-11 17:17:34 +0300

    Add axios and very initial public key fetch request

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,22 @@ import React, { useState } from 'react';
 import 'react-calendar/dist/Calendar.css';
 import Calendar from 'react-calendar';
 import './App.css';
-
+import axios from 'axios';
 const url = process.env.REACT_APP_ENVELOPE_BACKEND_URL;
 
 function App() {
   const [value, onChange] = useState(new Date());
 
+  const submit = async (event: any) => {
+    event.preventDefault();
+    const res = await axios.get(`${url}/publickey`);
+    console.log(res.data);
+  };
+
   return (
     <div className="grid place-items-center h-screen bg-gray-800 text-stone-200">
       <div className="rounded-md bg-cyan-600 p-3">
-        <form>
+        <form onSubmit={submit} >
           <div className="lex flex-col">
             <div className="flex-1 flex flex-col m-2">
               <textarea id="message"
