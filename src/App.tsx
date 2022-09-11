138ec4dc83d6e7f196684b6dcbeeed20b2f5efe1
Veikko Soininen <veikko.soininen@gmail.com>
2022-09-11 16:52:09 +0300

    Add Calendar. Some styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import 'react-calendar/dist/Calendar.css';
+import Calendar from 'react-calendar';
 import './App.css';
 
 const url = process.env.REACT_APP_ENVELOPE_BACKEND_URL;
 
 function App() {
+  const [value, onChange] = useState(new Date());
+
   return (
     <div className="grid place-items-center h-screen bg-gray-800 text-stone-200">
       <div className="rounded-md bg-cyan-600 p-3">
         <form>
-          <div className="flex flex-col m-2">
-            <div>
+          <div className="lex flex-col">
+            <div className="flex-1 flex flex-col m-2">
               <textarea id="message"
                 className="p-2.5 bg-gray-800 rounded-lg border"
                 placeholder="Your secret message...">
               </textarea>
+              <div className="mt-3">
+                <Calendar onChange={onChange} value={value} />
+              </div>
             </div>
             <div className="flex flex-row justify-center mt-2">
-              <input className="hover:underline cursor-pointer" type="submit" value="Encrypt" />
+              <input className="hover:underline cursor-pointer"
+                type="submit"
+                value="Encrypt" />
             </div>
           </div>
         </form>
