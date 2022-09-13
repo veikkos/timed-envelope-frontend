b593a849629135e7dfd346806ebd740047f5338c
Veikko Soininen <veikko.soininen@gmail.com>
2022-09-13 21:20:22 +0300

    Add very initial decryption support

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,53 @@ import 'react-calendar/dist/Calendar.css';
 import Calendar from 'react-calendar';
 import './App.css';
 import axios from 'axios';
+import { cryptico, RSAKey } from '@daotl/cryptico';
 const url = process.env.REACT_APP_ENVELOPE_BACKEND_URL;
 
 function App() {
-  const [value, onChange] = useState(new Date());
+  const [calendarDate, onChange] = useState(new Date());
+
+  async function encrypt(event: any) {
+    const plaintext = event.target[0].value;
+
+    if (plaintext.length) {
+      const res = await axios.get(`${url}/publickey/${calendarDate.toISOString()}`);
+      const key = res.data.key;
+      if (key) {
+        // @ts-ignore
+        const secret = cryptico.encrypt(plaintext, key, undefined);
+        if ("cipher" in secret) {
+          console.log(`${calendarDate.toISOString()}_${secret.cipher}`);
+        }
+      }
+    }
+  }
+
+  async function decrypt(event: any) {
+    const input = event.target[0].value;
+
+    if (input) {
+      const split = input.split("_");
+      const res = await axios.get(`${url}/privatekey/${split[0]}`);
+      const key = res.data.key;
+      if (key) {
+        const rsaKey = RSAKey.parse(key)
+        if (rsaKey) {
+          const plaintext = cryptico.decrypt(split[1], rsaKey);
+          console.log(plaintext);
+        }
+      }
+    }
+  }
 
   const submit = async (event: any) => {
     event.preventDefault();
-    const res = await axios.get(`${url}/publickey`);
-    console.log(res.data);
+    console.log(event.nativeEvent.submitter.name);
+    if (event.nativeEvent.submitter.name === "encrypt") {
+      await encrypt(event);
+    } else {
+      await decrypt(event);
+    }
   };
 
   return (
@@ -25,13 +63,18 @@ function App() {
                 placeholder="Your secret message...">
               </textarea>
               <div className="mt-3">
-                <Calendar onChange={onChange} value={value} />
+                <Calendar onChange={onChange} value={calendarDate} />
               </div>
             </div>
-            <div className="flex flex-row justify-center mt-2">
+            <div className="flex flex-row justify-evenly mt-2">
               <input className="hover:underline cursor-pointer"
                 type="submit"
+                name="encrypt"
                 value="Encrypt" />
+              <input className="hover:underline cursor-pointer"
+                type="submit"
+                name="decrypt"
+                value="Decrypt" />
             </div>
           </div>
         </form>
