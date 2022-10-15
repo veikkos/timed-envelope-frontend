d24c10d09c7094ab29059f20e2fb46ef9034c368
Veikko Soininen <veikko.soininen@gmail.com>
2022-10-15 10:07:19 +0300

    Background blur. Styling

--- src/Modal.tsx
+++ src/Modal.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 function Modal(props: Props) {
     return (
-        <div className={(props.visible ? 'fade-in' : 'fade-out') + " absolute bg-black bg-opacity-70 h-full w-full flex justify-center items-center p-8"}>
+        <div className={(props.visible ? 'fade-in' : 'fade-out') + " inactive-background flex-center p-8"}>
             <div className="modal rounded-md bg-cyan-600 shadow-lg shadow-cyan-900 text-gray-200 font-bold text-md p-6">
                 <div className="mb-8">
                     {props.text}
--- src/Spinner.tsx
+++ src/Spinner.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 function Spinner(props: Props) {
     return (
-        <div className={(props.visible ? 'fade-in' : 'fade-out') + " spinner absolute bg-black bg-opacity-70 h-full w-full flex justify-center items-center"}>
+        <div className={(props.visible ? 'fade-in' : 'fade-out') + " spinner inactive-background flex-center"}>
             <div className="text-gray-200 font-bold text-xl loader">
                 Loading<span className="loader__dot">.</span><span className="loader__dot">.</span><span className="loader__dot">.</span>
             </div>
